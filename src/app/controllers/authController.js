324838a05c2c10b2a9180fbdba405f096c9533af
Jairo Caetano <jairojunior841@gmail.com>
2021-03-01 10:07:35 -0300

    update

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const path = require("path");
 
 const mailer = require("../../modules/mailer");
 
